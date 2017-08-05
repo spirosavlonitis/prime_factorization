2b2ce5207ea0cc4321e577de6839e45393847013
Spiros Avlonitis <spirosa84@hotmail.com>
2017-08-05 17:14:37 +0300

    first working version

--- lib/prime_factorization.rb
+++ lib/prime_factorization.rb
@@ -1,15 +1,74 @@
 require "prime_factorization/version"
 
 module PrimeFactorization
-  def all_factors(number)
+  def factors(number)
   		result=[]
-  		i=1
+  		i=2
   		while i < number
   			result << i if number % i == 0
   			i+=1
   		end
-  		return result << number
+  		return result
   end
 
+  def prime_factors(number)
+  	  	all_factors=factors(number)
+  	  	result=[]
+  	  	all_factors.each do |factor|
+  	  		if result.size == 0
+  	  			result << factor
+  	  		else
+  	  			val=true
+  	  			result.each do |r|
+  	  				val = false if factor % r == 0
+  	  			end
+  	  			result << factor if val
+  	  		end
+  	  	end
+  	  	return result
+  end
+
+  def prime_factorization(number)
+  		primes = prime_factors(number)
+
+  		if primes.size == 0
+  			return "#{number} is a prime number"
+  		else
+  			used_primes={}
+  			primes.each do |prime|
+  				x=1
+  				while number > 1
+  					if number % prime == 0
+  						used_primes.[]=(prime,x)
+  						x+=1
+  						number /= prime
+  					else
+  						break
+  					end
+  				end
+  			end
+  			result=""
+  			powers = {2=>"\u00b2",3=>"\u00b3",4=>"\u2074",5=>"\u2075",
+			6=>"\u2076",7=>"\u2077",8=>"\u2078",9=>"\u2079"}
+
+			used_primes.each do |key,value|
+				if value > 0
+					if value == 1
+						result << "#{key} * "
+					elsif value < 9
+						result << "#{key}#{powers[value]} * "
+					else
+						result << "#{key}^#{value} * "
+					end
+				end
+			end
+
+			result.chomp!(" * ")
+
+  		end
+  		
+  		return result
+
+  end
 
 end
--- spec/prime_factorization_spec.rb
+++ spec/prime_factorization_spec.rb
@@ -1,11 +1,27 @@
 require "spec_helper"
 
 RSpec.describe PrimeFactorization do
+  let(:prime) { Class.new { include PrimeFactorization } }
+  let(:obj) { prime.new}
   it "has a version number" do
     expect(PrimeFactorization::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(PrimeFactorization.new.all_factors(24)).to eq([1,2,3,4,6,8,12,24])
+  it "Returns all factors except the number used and 1" do
+    expect(obj.factors(24)).to eq([2,3,4,6,8,12])
   end
+  
+  it "Returns only prime factors" do
+  	expect(obj.prime_factors(24)).to eq([2,3])
+  end
+
+  it "Checks to see if the number given is a prime" do
+  		expect(obj.prime_factorization(41)).to eq("41 is a prime number")
+  end
+
+  it "Returns the prime factorization of a number" do
+  		expect(obj.prime_factorization(24)).to eql("2\u00b3 * 3")
+  end
+
+
 end
